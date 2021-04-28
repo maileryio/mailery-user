50417b91e40406bcbef5e64325a8cab84f2f4a54
Igor Maliy <imaliy.filsh@gmail.com>
2021-04-28 17:57:17 +0300

    Development

--- views/default/view.php
+++ views/default/view.php
@@ -53,7 +53,7 @@ $this->setTitle($user->getUsername());
 </div>
 <div class="mb-2"></div>
 <div class="row">
-    <div class="col-12 grid-margin">
+    <div class="col-12">
         <?= DetailView::widget()
             ->data($user)
             ->options([
