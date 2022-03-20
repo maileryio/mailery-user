f498c3579557ad31d66f007f72da69bc36e31f63
Igor Maliy <imaliy.filsh@gmail.com>
2022-03-20 11:50:14 +0200

    Development

---
+++ config/rbac/items.php
@@ -0,0 +1,12 @@
+<?php
+
+use Mailery\User\Enum\Rbac as UserRbac;
+
+return [
+    UserRbac::ROLE_ADMIN => [
+        'name' => UserRbac::ROLE_ADMIN,
+        'type' => 'role',
+        'updatedAt' => 1599036348,
+        'createdAt' => 1599036348,
+    ],
+];
