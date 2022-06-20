4fc71b96a34cdc9a18130deb37c899e3ac501540
Igor Maliy <imaliy.filsh@gmail.com>
2022-06-20 23:16:25 +0300

    Development

--- views/auth/login.php
+++ views/auth/login.php
@@ -12,19 +12,23 @@ use Yiisoft\Form\Widget\Form;
 <div class="mb-4"></div>
 <div class="row">
     <div class="col-6 offset-3">
-        <?= Form::widget()
-                ->csrf($csrf)
-                ->id('login-form')
-                ->begin(); ?>
+        <div class="card mb-3">
+            <div class="card-body">
+                <?= Form::widget()
+                        ->csrf($csrf)
+                        ->id('login-form')
+                        ->begin(); ?>
 
-        <?= $field->text($form, 'login')->autofocus(); ?>
+                <?= $field->text($form, 'login')->autofocus(); ?>
 
-        <?= $field->password($form, 'password'); ?>
+                <?= $field->password($form, 'password'); ?>
 
-        <?= $field->submitButton()
-                ->class('btn btn-primary float-right mt-2')
-                ->value('Login'); ?>
+                <?= $field->submitButton()
+                        ->class('btn btn-primary float-right mt-2')
+                        ->value('Login'); ?>
 
-        <?= Form::end(); ?>
+                <?= Form::end(); ?>
+            </div>
+        </div>
     </div>
 </div>
